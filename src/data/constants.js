380b264729a19225efd52055a0c493b5367031eb
Vineeth Chivukula <qualityhacker2002@gmail.com>
2023-11-07 13:39:14 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,294 @@
+export const Bio = {
+  name: "Vineeth Chivukula",
+  roles: ["Photoshop Artist", "Video Editor", "Programmer"],
+  description:
+    "Much like Spidey swings through the city, I constantly seek fresh opportunities and self-improvement, embodying a sense of purpose and a dedication to personal growth akin to Peter Parker's commitment to protecting the city as Spider-Man.",
+  github: "https://github.com/VineethChivukula",
+  resume:
+    "https://drive.google.com/file/d/1mIKFm9quRmVV9oj6LSwxl82uOILlxwck/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/VineethChivukula/",
+  twitter: "https://twitter.com/6nnyverse",
+  insta: "https://www.instagram.com/6nnyverse/",
+  youtube: "https://www.youtube.com/@6nnyverse361",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "ReactJS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image: "https://img.icons8.com/?size=256&id=kg46nzoJrmTR&format=png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Adobe Photoshop",
+        image:
+          "https://3.bp.blogspot.com/-5U6_MJ-dGlE/V6bnv0CP3EI/AAAAAAAAAgY/-g7WlJ5u-uU7Rqle1qvSM-aoN8CWHZ7yQCLcB/s1600/cs6.png",
+      },
+      {
+        name: "Adobe Illustrator",
+        image:
+          "https://www.vispronet.com/blog/wp-content/uploads/2018/10/512px-Adobe_Illustrator_CC_icon.svg_.png",
+      },
+      {
+        name: "Alight Motion",
+        image:
+          "https://th.bing.com/th/id/OIP.pIsN5JrVjjqQ9Uxx4Lx_RAHaHa?pid=ImgDet&rs=1",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.xidax.com/modules/pages/views/templates/front/xidax/img/logos/amazonlogo.png",
+    role: "Amazon ML Summer School 2023 Participant",
+    company: "Amazon ML Summer School India",
+    date: "Sep 2023 - Oct 2023",
+    desc: "Workied upon the fundamental concepts of key ML topics while linking them to practical industry applications.",
+    skills: [
+      "Supervised Learning",
+      "Deep Neural Networks",
+      "Dimensionality Reduction",
+      "Unsupervised Learning",
+      "Probabilistic Graphical Models",
+      "Sequential Learning",
+      "Casual Inferencing",
+      "Reinforcement Learning",
+    ],
+    // doc: "https://media.licdn.com/dms/image/D5622AQF9CfJDSQcAxg/feedshare-shrink_2048_1536/0/1694764084033?e=1702512000&v=beta&t=rA_klqMMXecpqJFaGfP7CZensp25A0zmolv3y0knPyY",
+  },
+  {
+    id: 0,
+    img: "https://junoonadventure.in/wp-content/uploads/2018/09/Make-a-Difference-Foundation.jpg",
+    role: "Academic Support Volunteer",
+    company: "Make A Difference",
+    date: "Dec 2022 - June 2023",
+    desc: "Mentored 10+ at-risk youth by implementing customized learning plans and improved their academic performance by 20% in Mathematics.",
+    skills: ["Communication", "Collaboration", "Time Management"],
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/D5603AQHhV5tf4pPaJA/profile-displayphoto-shrink_800_800/0/1668559877664?e=2147483647&v=beta&t=YFkLX5YoWqiBP-no_J9a73D9SwTi6g1Ohs9d7qqCrQc",
+    role: "Project Intern",
+    company: "HDLC Technologies",
+    date: "Feb 2023 - Mar 2023",
+    desc: "Developed a Web Application to calculate BMI using streamlit.",
+    skills: ["Python", "Streamlit"],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/R.01e345d46842858f1c7e5a2b7a8084e0?rik=kkRBDXiZ0CC%2f4g&riu=http%3a%2f%2fwww.ranklogos.com%2fwp-content%2fuploads%2f2014%2f10%2fVrsec-373x500.png&ehk=f01%2fIjRsH65Fq2rcUKyRbB87ilAEAsWevEfad3e%2bFPg%3d&risl=&pid=ImgRaw&r=0",
+    school: "Velagapudi Ramakrishna Siddhartha Engineering College",
+    date: "May 2020 - May 2024",
+    grade: "8.99 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at VRSEC, Vijayawada, where I have successfully completed six semesters of study. My academic curriculum has included essential courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, Computer Networks, and Cyber Security",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/256x256/96826ff7159f66e0ab8ef983b6ca3857",
+    school: "Narayana Junior College",
+    date: "Mar 2020",
+    grade: "9.79 CGPA",
+    desc: "I completed my class 12 high school education at Narayana Junior College N120, Kanuru.",
+    degree: "BIEAP(XII), MPC",
+  },
+  {
+    id: 2,
+    img: "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/256x256/96826ff7159f66e0ab8ef983b6ca3857",
+    school: "Narayana IIT Olympiad",
+    date: "Mar 2018",
+    grade: "10.00 CGPA",
+    desc: "I completed my class 10 high school education at Narayana IIT Olympiad, Vijayawada.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Identification Of Salt-affected Soils",
+    date: "May 2023 - Nov 2023",
+    description:
+      "I have successfully detected soil saline areas within Machilipatnam using Landsat 8 satellite imagery by implementing Random Forest (RF) Algorithm.",
+    image: "https://i.ibb.co/DVNqw42/RF.png",
+    tags: ["JavaScript", "GEE", "Remote Sensing", "Machine Learning"],
+    category: "web app",
+    github:
+      "https://code.earthengine.google.com/997b4253205f57e77248e9b50ec4ff91?noload=true",
+    // webapp: "",
+    member: [
+      {
+        name: "Vineeth Chivukula",
+        img: "https://avatars.githubusercontent.com/u/66107997?v=4",
+        linkedin: "https://www.linkedin.com/in/VineethChivukula/",
+        github: "https://github.com/vineethchivukula/",
+      },
+      {
+        name: "Naga Ganesh Kothagundla",
+        img: "https://media.licdn.com/dms/image/C5603AQEx4O7H4XDTag/profile-displayphoto-shrink_800_800/0/1660921717881?e=1704931200&v=beta&t=UswaxlvtHKKgAxFr4IUHYUO_n2--NTPCHYKjkuztEC8",
+        linkedin: "https://www.linkedin.com/in/naga1215/",
+        // github: "",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Simple Python IDE",
+    date: "Aug 2023",
+    description:
+      "I have succesfully developed a web application that allows users to run python code on the webpage.",
+    image: "https://i.ibb.co/WxxnjVr/ide.png",
+    tags: ["HTML", "CSS", "Node Js", "Express Js",],
+    category: "web app",
+    github: "https://github.com/VineethChivukula/Simple-Python-IDE/",
+    webpage: "http://vinny-ide.onrender.com/",
+  },
+  {
+    id: 0,
+    title: "Rice Crop Disease Detection Using xAI",
+    date: "Dec 2022 - May 2023",
+    description:
+      "I have introduced a custom CNN model for detecting diseases of a rice crop and explained using LIME Library.",
+    image:
+      "https://i.ibb.co/0tQX2HN/lime.png",
+    tags: [
+      "Python",
+      "Deep Learning",
+    ],
+    category: "web app",
+    github: "https://colab.research.google.com/drive/1PGqPkLn_7WCxiiw7Oa4XHrM6F3NE-k3k?usp=sharing",
+    // webapp: "https://vexa-app.netlify.app/",
+    member: [
+      {
+        name: "Vineeth Chivukula",
+        img: "https://avatars.githubusercontent.com/u/66107997?v=4",
+        linkedin: "https://www.linkedin.com/in/VineethChivukula/",
+        github: "https://github.com/vineethchivukula/",
+      },
+      {
+        name: "Naga Ganesh Kothagundla",
+        img: "https://media.licdn.com/dms/image/C5603AQEx4O7H4XDTag/profile-displayphoto-shrink_800_800/0/1660921717881?e=1704931200&v=beta&t=UswaxlvtHKKgAxFr4IUHYUO_n2--NTPCHYKjkuztEC8",
+        linkedin: "https://www.linkedin.com/in/naga1215/",
+      },
+      {
+        name: "Surya Naga Chandra Dhanekula",
+        img: "https://i.pinimg.com/736x/69/a5/60/69a5602fb6377d1fef9bb45e8db9e415.jpg",
+      },
+    ]
+  },
+  {
+    id: 1,
+    title: "BMI Calculator",
+    date: "Feb 2023 - Mar 2023",
+    description:
+      "I have developed a Web Application to calculate BMI using streamlit.",
+    image: "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ab05e8b6-ea42-49a5-a61c-4be97c78e437/Raw_App_Screenshot.png",
+    tags: ["Python", "Streamlit"],
+    category: "web app",
+    github: "https://github.com/VineethChivukula/BMI-Calculator",
+    webpage: "https://vinny-bmi.streamlit.app/",
+  },
+  {
+    id: 2,
+    title: "The Queen Collab",
+    date: "Sep 2021",
+    description:
+      "I have had the privilege of collaborating on an extraordinary art project with highly skilled artists on Instagram.",
+    image: "https://i.ibb.co/8M2T7db/collab.jpg",
+    tags: ["Adobe Photoshop"],
+    category: "web app",
+    webpage: "https://www.instagram.com/p/CUSrHrSobTy/?img_index=4",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
