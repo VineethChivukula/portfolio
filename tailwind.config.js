393efe70ef486c7bf56d520d080f3ae0597cec36
VineethChivukula <vineethonsocialmedia@gmail.com>
2024-09-24 13:30:05 +0530

    incorrect tailwind code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
